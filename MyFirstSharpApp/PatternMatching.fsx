82fdf43d1405a910893ebb1c6d4f2d2fdb0de12a
sambaranhazra <sambaran2010@gmail.com>
2018-09-23 06:50:33 +0530

    Updated the pattern matching.

--- MyFirstSharpApp/PatternMatching.fsx
+++ MyFirstSharpApp/PatternMatching.fsx
@@ -1,7 +1,26 @@
 let getCreditLimit customer = 
     match customer with
-    |   "medium", 1 -> 500
+    |   "medium", year when year > 1 -> 500
     |   "good", year when year < 2 -> 750
     |   "good", 2 -> 1000
     |   "good", _ -> 2000
     |   _ -> 250
+
+type Customer =
+    {Name:string; Balance: int}
+
+let handleCustomers (customers:Customer List) = 
+    match customers with
+    | []->failwith "No customers supplied"
+    | [customer] -> printfn "Single customer, name is %s" customers.Head.Name
+    | [customer1; customer2] -> printfn "Two customers, balance %d" (customer1.Balance + customer2.Balance)
+    | customers->printfn "Customers supplied %d" customers.Length
+
+let emptyCustomerList = List.empty<Customer>
+
+let singleCustomerList: Customer List = [{Name="Sambaran";Balance=100}]
+
+let twoCustomersList: Customer List = [{Name="Sambaran";Balance=100};{Name="Babai";Balance=100}]
+
+                                        
+let threeCustomersList: Customer List = [{Name="Sambaran";Balance=100};{Name="Babai";Balance=100};{Name="Jhunai";Balance=50}]
