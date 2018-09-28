5fa8dd56898afc4c813b8554fd27b2dcdea1454d
Sambaran Hazra <sambaran2010@gmail.com>
2018-09-28 21:57:40 +0530

    Discriminated union code added

---
+++ MyFirstSharpApp/DiscriminatedUnion.fsx
@@ -0,0 +1,22 @@
+﻿type CheckNumber = int
+type CardNumber = string
+type CardType = Visa | Mastercard
+type CreditcardInfo = CardNumber * CardType
+
+type PaymentMethod = 
+    | Cash
+    | Check of CheckNumber
+    | Card of CreditcardInfo
+
+
+type PaymentAmount = decimal
+type Currency = Euro | USD
+
+type Payment = {
+    Amount : PaymentAmount
+    Currency : Currency
+    Method : PaymentMethod
+}
+
+let a:PaymentAmount = decimal(10)
+let p ={ Amount=decimal(10); Currency=Euro;Method=Cash}
